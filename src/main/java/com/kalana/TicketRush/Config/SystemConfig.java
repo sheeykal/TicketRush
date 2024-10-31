a01f8ac2e04dc28784ff0e1c7137bf6bc072a0f9
Kalana Shehara <sheeykal@gmail.com>
2024-11-01 02:28:46 +0530

    Updated the SystemConfig Class

--- src/main/java/com/kalana/TicketRush/Config/SystemConfig.java
+++ src/main/java/com/kalana/TicketRush/Config/SystemConfig.java
@@ -1,4 +1,18 @@
 package com.kalana.TicketRush.Config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SystemConfig {
+    private int totalTicket;
+    private int ticketReleaseRate;
+    private int comsumerRetrevalRate;
+    private int ticketPoolCapacity;
+
+
+
 }
