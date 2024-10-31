095e9c147a5000370a0f76a2b829b72cf50ad273
Kalana Shehara <sheeykal@gmail.com>
2024-11-01 03:03:45 +0530

    Created SimulationService Class

--- src/main/java/com/kalana/TicketRush/Controller/SimulationController.java
+++ src/main/java/com/kalana/TicketRush/Controller/SimulationController.java
@@ -1,4 +1,18 @@
 package com.kalana.TicketRush.Controller;
 
+import com.kalana.TicketRush.Config.SystemConfig;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class SimulationController {
+
+
+    @PostMapping("/start-simulation")
+    public void getConfig(SystemConfig config)
+    {
+
+    }
 }
---
+++ src/main/java/com/kalana/TicketRush/service/SimulationService.java
@@ -0,0 +1,7 @@
+package com.kalana.TicketRush.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimulationService {
+}
