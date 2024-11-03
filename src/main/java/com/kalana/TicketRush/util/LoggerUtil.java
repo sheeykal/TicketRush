bdaf99c54571de2691786cbc813798e99a1eeb16
Kalana Shehara <sheeykal@gmail.com>
2024-11-04 03:53:12 +0530

    Added LoggerUtil

--- src/main/java/com/kalana/TicketRush/util/LoggerUtil.java
+++ src/main/java/com/kalana/TicketRush/util/LoggerUtil.java
@@ -1,2 +1,21 @@
-package com.kalana.TicketRush.util;public class LoggerUtil {
+package com.kalana.TicketRush.util;
+
+import com.kalana.TicketRush.model.LogEntry;
+import com.kalana.TicketRush.repository.LogEntryRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class LoggerUtil {
+
+    LogEntryRepo logEntryRepo;
+
+    public void log(String message)
+    {
+        logEntryRepo.save(new LogEntry(message));
+        System.out.println(("message: " + message));;
+
+        //make this return method to return the message so that controller can create a service obj and one it start the
+    }
 }
