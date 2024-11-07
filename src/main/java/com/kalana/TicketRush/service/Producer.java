589ba494fbf6be8f3c6c8fc2e3a6b195350d5c14
Kalana Shehara <sheeykal@gmail.com>
2024-11-08 01:22:42 +0530

    Updated Producer

--- src/main/java/com/kalana/TicketRush/service/Producer.java
+++ src/main/java/com/kalana/TicketRush/service/Producer.java
@@ -4,35 +4,41 @@ import com.kalana.TicketRush.model.Ticket;
 import com.kalana.TicketRush.repository.TicketRepo;
 import com.kalana.TicketRush.util.LoggerUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
 
-@Component
+
 @AllArgsConstructor
 public class Producer implements Runnable{
     private final TicketPool ticketPool;
     private final LoggerUtil logger;
     private final TicketRepo ticketRepo;
-    private final Integer producerId;
-    private final Interger ticketReleaseRate;
+    private final int producerId;
+    private final int ticketReleaseRate;
+
+
 
     @Override
-    public void run() {
-        try {
-            while (true) {
-                if(ticketPool.getCurrentTicketInthePool()< ticketPool.getTicketPoolCapacity()){
+    public void run()
+    {
+        try
+        {
+            while (true)
+            {
+                if(ticketPool.getCurrentTicketInthePool()< ticketPool.getTicketPoolCapacity())
+                {
                     Ticket ticket = new Ticket();
                     ticketPool.produceTicket(ticket);
                     ticketRepo.save(ticket);
-                    logger.log("Producer" + producerId + "added ticket" + ticket.getId());
+                    logger.log("Producer " + producerId + "added ticket" + ticket.getId());
                 }else
                 {
                     logger.log("Ticket pool is full; Producer" + producerId + "is waiting!");
-
                 }
                 Thread.sleep(ticketReleaseRate + 1000L);
             }
-        } catch (InterruptedException e){
-            throw new RuntimeException(e);
+        } catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+            System.out.println("Producer thread is interrupted:" + e.getMessage());
         }
 
     }
