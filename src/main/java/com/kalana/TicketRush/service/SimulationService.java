3883463deba9fc88c9eff73a075aee41d688aae4
Kalana Shehara <sheeykal@gmail.com>
2024-11-08 03:11:29 +0530

    Updated ServiceClass

--- src/main/java/com/kalana/TicketRush/service/SimulationService.java
+++ src/main/java/com/kalana/TicketRush/service/SimulationService.java
@@ -1,20 +1,45 @@
 package com.kalana.TicketRush.service;
 
 import com.kalana.TicketRush.Config.SystemConfig;
+import com.kalana.TicketRush.repository.TicketRepo;
+import com.kalana.TicketRush.util.LoggerUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimulationService {
 
-    public static void getConfig(SystemConfig config) {
+    private TicketPool ticketPool;
+    private Thread producerThread;
+    private Thread consumerThread;
+
+    private LoggerUtil logger;
+    private TicketRepo ticketRepo;
+
+    public SimulationService(LoggerUtil logger, TicketRepo ticketRepo)
+    {
+        this.logger = logger;
+        this.ticketRepo = ticketRepo;
     }
 
-    public void startSimulation(SystemConfig config) {
-        System.out.println("Simulation started");
-        System.out.println(config.toString());
+
+    public void startSimulation(SystemConfig config)
+    {
+        this.ticketPool = new TicketPool(config.getTicketPoolCapacity());
+        producerThread = new Thread(new Producer(ticketPool,logger,ticketRepo,101,config.getTicketReleaseRate()));
+        consumerThread = new Thread(new Consumer(ticketPool,logger,101,config.getComsumerRetrevalRate()));
+
+        producerThread.start();
+        consumerThread.start();
     }
 
-    public void stopSimulation() {
+
+    public void stopSimulation()
+    {
+        if(producerThread.isAlive() || consumerThread.isAlive())
+        {
+            producerThread.interrupt();
+            consumerThread.interrupt();
+        }
     }
 }
 
