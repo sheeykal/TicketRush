c1f922bc01102b6225c2a1573e9e21515f7b0419
Kalana Shehara <sheeykal@gmail.com>
2024-12-10 03:10:06 +0530

    added components

---
+++ frontend/src/app/initialconfig/initial-config/initial-config.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-config',
+  imports: [],
+  templateUrl: './initial-config.component.html',
+  styleUrl: './initial-config.component.css'
+})
+export class InitialConfigComponent {
+
+}
---
+++ frontend/src/app/mainpage/mainpage/mainpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage',
+  imports: [],
+  templateUrl: './mainpage.component.html',
+  styleUrl: './mainpage.component.css'
+})
+export class MainpageComponent {
+
+}
