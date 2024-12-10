fee697639627fa9d50dffe46a8ec25e9b38f9f09
Kalana Shehara <sheeykal@gmail.com>
2024-12-11 04:46:22 +0530

    Updated some stuff

--- src/main/java/com/kalana/TicketRush/Controller/SimulationController.java
+++ src/main/java/com/kalana/TicketRush/Controller/SimulationController.java
@@ -2,16 +2,23 @@ package com.kalana.TicketRush.Controller;
 
 import com.kalana.TicketRush.Config.SystemConfig;
 import com.kalana.TicketRush.service.SimulationService;
+import com.kalana.TicketRush.util.LoggerUtil;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ticket-rush")
+@CrossOrigin
 public class SimulationController {
 
-    private final SimulationService simulationService;
+    private final SimulationService  simulationService;
+    private final LoggerUtil loggerUtil;
 
-    public SimulationController(SimulationService simulationService) {
+    public SimulationController(SimulationService simulationService, LoggerUtil loggerUtil)
+    {
         this.simulationService = simulationService;
+        this.loggerUtil = loggerUtil;
     }
 
     @GetMapping("/")
@@ -27,14 +34,20 @@ public class SimulationController {
     }
 
     @PostMapping("/start-simulation")
-    public String startSimulation(@RequestBody SystemConfig config)
+    public void startSimulation(@RequestBody SystemConfig config)
     {
-        return simulationService.startSimulation(config);
+        System.out.println(config.toString());
+        simulationService.startSimulation(config);
     }
 
     @PostMapping("/stop-simulation")
     public String stopSimulation()
     {
         return simulationService.stopSimulation();
+    }
+
+    @GetMapping("/logs")
+    public List<String> getLogs() {
+        return loggerUtil.getLogs();
     }
 }
--- src/main/java/com/kalana/TicketRush/util/LoggerUtil.java
+++ src/main/java/com/kalana/TicketRush/util/LoggerUtil.java
@@ -3,25 +3,31 @@ package com.kalana.TicketRush.util;
 import com.kalana.TicketRush.model.LogEntry;
 import com.kalana.TicketRush.repository.LogEntryRepo;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class LoggerUtil {
 
+
     @Autowired
     LogEntryRepo logEntryRepo;
 
-    public LoggerUtil() {
+    @Getter
+    private List<String> logs = new ArrayList<>();
 
+    public LoggerUtil() {
     }
 
     public void log(String message)
     {
+        logs.add(message);
         logEntryRepo.save(new LogEntry(message));
-        System.out.println(("message: " + message));;
-
-        //make this return method to return the message so that controller can create a service obj and one it start the
+        System.out.println(("Log : " + message));;
     }
 }
